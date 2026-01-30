95a83b473db5fab7bbe709f1a07bb1f8cdc45ea8
gabriel <gabriel.vieirafonseca5@gmail.com>
2026-01-30 20:44:57 -0300

    mudança

--- desafios/alura.py
+++
@@ -1,8 +0,0 @@
-#print('Olá,Mundo!')
-#import this
-idade = int(input('Qual sua idade? '))
-
-if idade >= 18:
-    print('Você pode tirar sua cnh!')
-else:
-    print('Você ainda não pode tirar a cnh')
